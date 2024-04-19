c7eadf903248f41bdf3cdc252e539ec899cd51c3
Nicolas Le Borgne <nicolas.leborgne@me.com>
2024-04-19 23:47:21 +0200

    v1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
     return (
         <>
-            <p className="absolute top-2 left-2 text-slate-700">4</p>
-            <p className="absolute left-2 bottom-2 text-slate-700">5</p>
-            <p className="absolute top-2 right-2 text-slate-700">1</p>
-            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <p className="absolute top-2 left-2 animate-blinkingText">4</p>
+            <p className="absolute left-2 bottom-2 animate-blinkingText">5</p>
+            <p className="absolute top-2 right-2 animate-blinkingText">1</p>
+            <main className="grid place-items-center h-screen text-center">
                 <h1 className="text-6xl">
                     ÊTES VOUS ASSEZ <span className="char">ATTENTIF</span> ?
                 </h1>
